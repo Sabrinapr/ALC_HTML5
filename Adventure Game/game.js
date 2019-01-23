377f9a3a0a4f23631a3b7ba79442b9b05e35bd71
Unknown <sabrina.p338@stu.nebo.edu>
2019-01-23 10:27:08 -0700

    learning!

---
+++ Adventure Game/game.js
@@ -0,0 +1,15 @@
+// single line comment
+/*
+
+multiline comment
+
+*/
+
+//document.write("<h1>I'm <em>very</em> tired</h1>");
+
+// alert("Warning! I can and will fight you... Warning!");
+// confirm("do you like pokemon?");
+// prompt("what type of pokemon do you like?");
+
+var pc = prompt("What is your name?");
+confirm("SO your name is "+pc); 
