7dea938cd64c439ab257894e707f1a6c3f16ab18
Unknown <sabrina.p338@stu.nebo.edu>
2019-02-15 21:42:55 -0700

    Working on jackdaw route.  STILL DON'T KNOW WHY THE OBJECTS R NOT WORKIN

--- Adventure Game/game.js
+++ Adventure Game/game.js
@@ -22,12 +22,14 @@ multiline comment
 //<script>
 //javascript for inventory
 var inventory = {
-            bat: 0,
-            keys: 0,
-            food: 0,
-            money: 0,
+            bat:0,
+            keys:0,
+            food:0,
+            coins:100,
             
         }
+
+	
 
 function GetRandInt(max){
 	var randInt = Math.floor(Math.random()* Math.floor(max));
@@ -135,7 +137,8 @@ function Game(){
             alert("You walk out into another hallway, but this one is full of doors.  You are very obviously in an apartment complex, so you figure the rest of the doors will be locked.  You move downstairs.");
            MainOffice();
     
-        //takes money out of account inventory.coins = inventory.coins - 100; inventory.coins -=100; alert("you have " +inventory.coins+"coins remaining"); 
+        //takes money out of account 
+               inventory.coins = inventory.coins - 100; inventory.coins -=100; alert("you have " +inventory.coins+"coins remaining"); 
         }
         
         if(stairview == "look at table" || stairview == "table"){
@@ -143,12 +146,21 @@ function Game(){
             KitchenRoom();
         }
         if(stairview == "take key" || stairview == "key"){
+            var keyTake =confirm("you take the key.  It seems to belong to inside this apartment.");
+            if(keyTake){
         //adds key
-            inventory.keys +1; 
-           // alert("you own " +inventory.key+"keys"); 
-           //else{
-               //alert("You think the key might go to the door upstairs.");
+            inventory.keys ++; 
+                //displays keys taken
+           alert("you own " +inventory.key+" keys"); 
+           KitchenRoom();
+            }
+                else{
+               alert("You think the key might go to the door upstairs.");
                KitchenRoom();
+        }
+        
+            
+        
         }
    if(stairview == "upstairs" || stairview == "go back"){
        Hallway();
@@ -211,12 +223,71 @@ function Game(){
                 Crew();
             }
             else{
-                alert("ou pissed off the the leader of the group you encountered somehow. Good job idiot."); 
+                alert("You pissed off the the leader of the group you encountered somehow. Good job idiot."); 
                 Alone();
             }
    }
     function Crew(){
-        alert("You seemed to have pleased the leader.  Guess that means you are with them now?")
+       var nameAsk = prompt("You seemed to have pleased the leader.  Guess that means you are with them now? “You seem like you can handle yourself.  Welcome to the crew friend, what do we call call you?” The leader extends a hand to you.");
+        alert("Good to meet ya "+nameAsk+".")
+        alert("You walk with the leader to a car that is still functioning unlike the others you have seen. There is actually a couple cars that everybody seems to be getting in to.They drive a while away from the city, attracting a couple zombies after them, but none of the zombies move fast enough to catch up to the car.  The journey comes to an end as they stop at a heavily barracaded building, full of anything you would need to stop a horde of zombies from invading.");
+        alert("They shove you into a room upstairs, and say “Rest up and we will do introductions tomorrow.  And explain everything.”");
+        Hideout();
+    }
+    function Alone(){
+        prompt("“It seems they are too stupid to survive long enough out here, let’s scram.” The group leader says to the rest of her friends while glaring at you. They walk off, leaving you alone in the desolate city.  You can go in any direction here.");
+
+    }
+    function Hideout(){
+        var maskPick = prompt("You are in a very comfortable looking room, and even though you where asleep a couple hours ago, you feel some exhaustion. The bed is obviously comfy, but you are in an unfamiliar place. /- go to bed /- open door ");
+        if(maskPick == "go to bed" || maskPick == "bed"){
+            Bunny();
+        }
+        if(maskPick == "open door"){
+            alert("You decide to wait till they all fall asleep to explore. After about an hour you decide you are safe to explore.")
+            Hallwayv1();
+        }
+    }
+    function Hallwayv1(){
+        var way = prompt("The hallway has 2 other doors to the left, the right, and in front stairs leading downstairs.");
+        switch(way){
+            case "left":
+                alert("you open the door with ease. there seems to be someone who is... sleeping? with the door unlocked? whatever. you leave them alone.");
+                break;
+            case "right":
+                alert("The door is locked. I wouldn't suggest slamming yourself against it like last time.");
+                break;
+            case "open door with key":
+                Traproom();
+                break;
+            case "go foward"||"downstairs":
+                Kitchen();
+        }
+    }
+    function Traproom(){
+        var lazars = prompt("You enter a room that is completely vacant, besides the table in the middle of the room.  Suprisingly there are dection lazars, everywhere.");
+        switch(lazars){
+            case "look around":
+            alert("On the top of that table are two masks with lights on them, almost like they are being displayed.  It is a bird mask of sorts and a bunny mask with a symbol on the forehead.  Why they are protecting masks, who knows. There is a keypad by the door.");
+            Traproom();
+            break;
+            case "look at keypad"||"keypad":
+            prompt("its a keypad with a 5 number passcode needed.  What's the passcode?");
+            if("65910"){
+                prompt("A light on the keypad turns green. The lazars behind you shut off");
+                Traproomv2();
+            }
+            else{
+                Killed(); 
+            }
+        break;
+        case "take masks":
+        alert("an alarm goes off.");
+        Killed();
+    }
+    }
+    function Traproomv2(){
+        
     }
 }
 
